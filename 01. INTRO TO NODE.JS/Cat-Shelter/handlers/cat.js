6682d88ea8f53e0c0dfb48f25b55c34323f3e56e
HristoMachikov <hristo2635@abv.bg>
2019-09-25 17:02:38 +0300

    edit catShelter

--- 01. INTRO TO NODE.JS/Cat-Shelter/handlers/cat.js
+++ 01. INTRO TO NODE.JS/Cat-Shelter/handlers/cat.js
@@ -63,7 +63,7 @@ module.exports = (req, res) => {
                 })
             });
 
-            res.writeHead(200, { localhost: '/' });
+            res.writeHead(302, { location: '/' });
             res.end();
         });
 
@@ -73,26 +73,113 @@ module.exports = (req, res) => {
 
         form.parse(req, (err, fields, files) => {
             if (err) throw err;
-            
+
             let oldPath = files.upload.path;
             let newPath = path.normalize(path.join(__dirname, '../', 'content/images/' + files.upload.name));
-            // console.log(oldPath);
-            // console.log(newPath);
             fs.rename(oldPath, newPath, (err) => {
                 if (err) throw err;
                 console.log('Filles was upload successfully!');
             });
 
-            fs.readFile('./data/cats.json', 'utf8', (err, data) => {
+            fs.readFile('./data/cats.json', 'utf-8', (err, data) => {
                 if (err) throw err;
                 let allCats = JSON.parse(data);
-                allCats.push({ ...fields, id: cats.length + 1, image: files.upload.name });
+                allCats.push({ id: allCats.length + 1, ...fields, image: files.upload.name });
                 let json = JSON.stringify(allCats);
                 fs.writeFile('./data/cats.json', json, () => {
-                    res.writeHead(200, { localehost: '/' });
+                    res.writeHead(302, { location: '/' });
+                    // res.redirect("/");
+                    res.end();
                 });
             });
         });
+    } else if (pathname.includes('/cats-edit') && req.method === 'GET') {
+        let filePath = path.normalize(path.join(__dirname, '../views/editCat.html'));
+        const index = fs.createReadStream(filePath);
+        index.on('data', (data) => {
+            let catId = req.url.split('/')[2];
+            let currentCat = cats.filter((cat) => cat.id === catId);
+            let modifiedData = data.toString().replace('{{id}}', catId);
+            modifiedData = modifiedData.replace('{{name}}', currentCat.name);
+            modifiedData = modifiedData.replace('{{description}}', currentCat.description);
+            let catBreedPlaceholder = breeds.map((breed) => `<option value="${breed}">${breed}</option>`);
+            modifiedData = modifiedData.replace('{{catBreeds}}', catBreedPlaceholder.join('\n'));
+            res.write(modifiedData);
+        });
+
+        index.on('end', () => {
+            res.end();
+        });
+
+        index.on('error', (err) => {
+            console.log(err);
+        });
+
+    } else if (pathname.includes('/cats-find-new-home') && req.method === 'GET') {
+
+        let filePath = path.normalize(path.join(__dirname, '../views/catShelter.html'));
+        const index = fs.createReadStream(filePath);
+        index.on('data', (data) => {
+            let catId = req.url.split('/')[2];
+            let currentCat = cats.filter((cat) => cat.id === catId);
+            let currentImg = path.join('./content/images/' + currentCat.image);
+            let modifiedData = data.toString().replace('{{id}}', catId);
+            modifiedData = modifiedData.replace('{{image}}', currentImg);
+            modifiedData = modifiedData.replace('{{name}}', currentCat.name);
+            modifiedData = modifiedData.replace('{{description}}', currentCat.description);
+            modifiedData = modifiedData.replace('{{breed}}', currentCat.breed);
+            res.write(modifiedData);
+        });
+
+        index.on('end', () => {
+            res.end();
+        });
+
+        index.on('error', (err) => {
+            console.log(err);
+        });
+
+
+    } else if (pathname.includes('/cats-edit') && req.method === 'POST') {
+
+        let form = new formidable.IncomingForm();
+
+        form.parse(req, (err, fields, files) => {
+            if (err) throw err;
+
+            fs.readFile('./data/cats.json', 'utf-8', (err, data) => {
+                if (err) throw err;
+                let allCats = JSON.parse(data);
+                let catId = req.url.split('/')[2];
+                let editedCats = allCats.forEach((item, index, arr) => {
+                    if (index === +catId) {
+                        arr[index] = { ...fields };
+                    }
+                });
+                let json = JSON.stringify(editedCats);
+                fs.writeFile('./data/cats.json', json, () => {
+                    res.writeHead(302, { location: '/' });
+                    // res.redirect("/");
+                    res.end();
+                });
+            });
+        });
+
+    } else if (pathname.includes('/cats-find-new-home') && req.method === 'POST') {
+
+        fs.readFile('./data/cats.json', 'utf-8', (err, data) => {
+            if (err) throw err;
+            let allCats = JSON.parse(data);
+            let catId = req.url.split('/')[2];
+            let remainCats = allCats.filter((cat) => cat.id !== catId);
+            let json = JSON.stringify(remainCats);
+            fs.writeFile('./data/cats.json', json, () => {
+                res.writeHead(302, { location: '/' });
+                // res.redirect("/");
+                res.end();
+            });
+        });
+
     } else {
         return true;
     }
