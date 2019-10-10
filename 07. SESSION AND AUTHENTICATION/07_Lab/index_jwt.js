472f176c8042a93d0cc8281dfd39efd65b896c80
HristoMachikov <hristo2635@abv.bg>
2019-10-10 17:03:35 +0300

    solve 07_Lab

--- 07. SESSION AND AUTHENTICATION/07_Lab/index_jwt.js
+++ 07. SESSION AND AUTHENTICATION/07_Lab/index_jwt.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
+// const session = require('express-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3000;
+
+const options = { expiresIn: '2d' };
+const secret = 'MySuperPrivateSecret';
+
 const saltRounds = 9;
 
 function auth(req, res, next) {
-    const authUser = users.find(user => user.id === req.session.userId);
+    const token = req.cookies['auth_cookie'];
+    if (!token) {
+        res.status(401).send("401");
+        return;
+    }
+    const decodedToken = jwt.verify(token, secret);
+    const authUser = users.find(user => user.id === decodedToken.userId);
     if (!authUser) {
         res.status(401).send("401");
         return;
@@ -29,11 +40,11 @@ let users = [
 
 app.use(cookieParser());
 
-app.use(session(
-    { secret: 'my secret' },
-    { httpOnly: true },
-    { secure: true }
-));
+// app.use(session(
+//     { secret: 'my secret' },
+//     { httpOnly: true },
+//     { secure: true }
+// ));
 app.use(bodyParser.urlencoded());
 
 app.get('/protected', auth, (req, res) => {
@@ -41,22 +52,22 @@ app.get('/protected', auth, (req, res) => {
 })
 
 app.get('/logout', (req, res) => {
-    res.session.destroy((err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send(err.message);
-            return;
-        }
-        res.redirect('/');
-    });
+    //blacklist
+    res.clearCookie('auth_cookie').redirect('/');
+    // res.session.destroy((err) => {
+    //     if (err) {
+    //         console.error(err);
+    //         res.status(500).send(err.message);
+    //         return;
+    //     }
+    //     res.redirect('/');
+    // });
 });
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve('pages', 'register.html'));
 });
 
-
-//bcrypt
 app.post('/register', (req, res, next) => {
     const { username, password } = req.body;
     const user = users.find(user => user.username === username);
@@ -65,23 +76,23 @@ app.post('/register', (req, res, next) => {
         return;
     }
     bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(password, salt, (err, hash) => {
-            if (err) {
-                next(err);
-                return;
-            }
-            users = users.concat({ id: 2, username, password: hash });
-            res.redirect('/');
-        })
-    });
+    bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        users = users.concat({ id: 2, username, password: hash });
+        res.redirect('/');
+    })
+    })
+
+
 });
-//
 
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve('pages', 'login.html'));
 });
 
-//bcrypt
 app.post('/login', (req, res, next) => {
     const authUser = users.find(user => user.username === req.body.username);
     if (!authUser) {
@@ -93,45 +104,18 @@ app.post('/login', (req, res, next) => {
             res.sendFile(path.resolve('pages', 'login.html'));
             return;
         }
-        req.session.userId = authUser.id;
+        const token = jwt.sign({ userId: authUser.id }, secret, options);
+        res.cookie('auth_cookie', token);
+        // req.session.userId = authUser.id;
         res.redirect('/');
     }).catch(next);
 });
-//
-
-// app.post('/login', (req, res) => {
-//     const authUser = users.find(user => user.username === req.body.username);
-//     if (!authUser) {
-//         res.sendFile(path.resolve('pages', 'login.html'));
-//         return;
-//     }
-//     req.session.userId = authUser.id
-//     res.redirect('/');
-// });
 
 
 app.get('/', (req, res) => {
     res.cookie('test_cookie', { TEST: 123 });
     res.send('Hello World!');
 });
-
-
-app.get('/1', (req, res) => {
-    res.cookie('test_cookie_1', { TEST: 123111 }).send('1');
-});
-
-app.get('/readCookie', (req, res) => {
-    res.json(req.cookies);
-})
-
-//create session
-app.get('/setSession', (req, res) => {
-    req.session.message = "hello"
-    res.end('Session set')
-})
-app.get('/readSession', (req, res) => {
-    res.json(req.session)
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
